39b1d05acc2646e1b36cd0945bd8da4de43f5915
Briony Frater <briefrater@gmail.com>
2022-10-31 00:13:14 -0500

    Added Js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,22 @@
 /* Add your JavaScript to this file */
+
+window.onload = function(){
+    subBtn = document.body.getElementsByClassName("btn")[1];
+    msg = document.body.getElementsByClassName("message")[0];
+  
+
+    subBtn.addEventListener("click", function(event){
+        event.preventDefault();
+        emailVal = document.getElementById("email").value;
+        console.log(emailVal);
+        if(emailVal == ""){
+            msg.textContent = "Please enter a valid email address.";
+        }else{
+            msg.textContent = `Thank you! Your email address ${emailVal} has been added to our mailing list!`;
+        }
+        
+    });
+
+};
+
+    
